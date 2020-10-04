afc3360329f27a36b75a6096ea879cfd024c831e
MinhTuan <minhtuanvh99@gmail.com>
2020-10-04 21:40:51 +0700

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -9,15 +9,33 @@ connect.then((db) => {
   console.log("Connected correctly to server");
 
   Dishes.create({
-    name: "Uthapizza",
-    description: "Test",
+    name: "Uthappizza",
+    description: "test",
   })
     .then((dish) => {
       console.log(dish);
-      return Dishes.find({}).exec();
+
+      return Dishes.findByIdAndUpdate(
+        dish._id,
+        {
+          $set: { description: "Updated test" },
+        },
+        {
+          new: true,
+        }
+      ).exec();
     })
-    .then((dishes) => {
-      console.log(dishes);
+    .then((dish) => {
+      console.log(dish);
+      dish.comments.push({
+        rating: 5,
+        comment: "I'm getting a sinking feeling!",
+        author: "Leonardo di Carpaccio",
+      });
+      return dish.save();
+    })
+    .then((dish) => {
+      console.log(dish);
       return Dishes.remove({});
     })
     .then(() => {
@@ -31,12 +49,10 @@ connect.then((db) => {
     .save()
     .then((dish) => {
       console.log(dish);
-
       return Dishes.find({});
     })
     .then((dishes) => {
       console.log(dishes);
-
       return Dishes.remove({});
     })
     .then(() => {
