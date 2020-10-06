5599e2f89b2a8b4073414f7df5f2da1d40910792
MinhTuan <minhtuanvh99@gmail.com>
2020-10-06 13:10:02 +0700

    File Upload

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -8,6 +8,7 @@ var FileStore = require("session-file-store")(session);
 var passport = require("passport");
 var authenticate = require("./authenticate");
 var config = require("./config");
+const uploadRouter = require("./routes/uploadRouter");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -64,6 +65,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
