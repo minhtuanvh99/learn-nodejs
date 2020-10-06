5d3fc3150e51f4fc83a02136fc2c111811aa9946
MinhTuan <minhtuanvh99@gmail.com>
2020-10-06 22:15:42 +0700

    assignment 4

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,6 +15,7 @@ var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
+var favoriteRouter = require("./routes/favoriteRouter");
 
 const mongoose = require("mongoose");
 
@@ -65,6 +66,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use("/favorites", favoriteRouter);
 app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
